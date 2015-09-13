e7b5222aeaef91a60a69a3289138839130ec183e
sonimanoj88 <sonimanoj88@gmail.com>
2015-09-13 23:04:42 +0530

    changes done for categories

--- marketRoadWeb/WebContent/javaScripts/mainCtrl.js
+++ marketRoadWeb/WebContent/javaScripts/mainCtrl.js
@@ -1,6 +1,12 @@
 (function(){
-	angular.module("com.marketRoad").controller('mainCtrl',['$scope', function($scope){
+	angular.module("com.marketRoad").controller('mainCtrl',['$scope','homeService', function($scope, homeService){
 		
-		
+		$scope.categoryDetails = {};
+		$scope.categoryDet = function(){
+			homeService.getCategoryDetails().$promise.then(function(data){
+				$scope.categoryDetails = data;
+			});
+		}
+		$scope.categoryDet();
 	}]);
 })();	
